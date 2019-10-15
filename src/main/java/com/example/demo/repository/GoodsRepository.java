dd7ce088848e66bdefcff62bb300bafca958e1ae
lichengrong <lichengrong@qipeipu.com>
2019-10-15 20:43:09 +0800

    test

---
+++ src/main/java/com/example/demo/repository/GoodsRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.pojo.GoodsInfo;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface GoodsRepository extends ElasticsearchRepository<GoodsInfo,Long> {
+}
